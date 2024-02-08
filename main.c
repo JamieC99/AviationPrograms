641cebb9fe50dc8099156d37b838e240af13ed20
Jamie Coleman <jamie.h.coleman@gmail.com>
2024-02-09 12:51:07 +1300

    Added function to return to main menu
    The mainMenu function is declared inside main.c which contains the functionality there. An empty declaration is also present in pressalt.c without importing main.c to prevent a compile loop.

--- main.c
+++ main.c
@@ -1,120 +1,27 @@
 #include <stdio.h>
 #include <windows.h>
+#include "pressalt.c"
 
-#define standardPressure 1013
-#define standardTemperature 15
-
-int QNH;
-int QNHDiff;
-int aerodromeElevation;
-int pressureAltitude;
-int densityAltitude;
-
-void calcPressureAltitude(void);
-void calcDensityAltitude(void);
-void showHelp(void);
-
-void showOptions(void)
+void mainMenu(void)
 {
-    printf("\n\n1) Calculate\n2) Help\n3) Quit\n");
+    system("cls");
 
-    // Show options
-    int option;
-    scanf("%i", &option);
+    printf("AVIATION PROGRAMS\n\n");
+    printf("Would calculation would you like to perform?\n\n1) Density Altitude\n2)\n3) Quit\n");
 
-    switch(option)
+    int choice = scanf("%i", &choice);
+
+    switch(choice)
     {
-        case 1: calcPressureAltitude(); break;
-        case 2: showHelp(); break;
-        case 3: exit(0); break;
+        case 1: densityAltitudeMenu(); break;
+        case 2: break;
+        case 3: exit(0);
     }
-}
-
-// Main Menu
-void startMenu(void)
-{
-    system("cls");
-    printf("DENSITY ALTITUDE CALCULATOR");
-    showOptions();
-}
-
-void showHelp(void)
-{
-    system("cls");
-
-    printf("HELP\n\n");
-    printf("This program calculates the pressure altitude of an aerodrome,\n");
-    printf("as well as the density altitude at the aerodrome's elevation\n\n");
-    printf("Pressure altitude formula: Aerodrome Elevation - Difference in QNH * 30\n\n");
-    printf("Density altitude formula:\n");
-    printf("Aerodrome Pressure = 15 - (2 * Aerodrome Elevation / 1000)\n");
-    printf("Temperature Deviation = Surface Temperature - Aerodrome Temperature\n");
-    printf("Density Altitude = Pressure Altitude + Temperature Deviation * 120");
-
-    showOptions();
-}
-
-int absoluteValue(int value)
-{
-    if (value < 0) value = -value;
-    return value;
-}
-
-void calcPressureAltitude(void)
-{
-    system("cls");
-
-    printf("PRESSURE ALTITUDE\n\n");
-
-    // Calculate the difference in QNH
-    printf("Enter the QNH (hPa): ");
-    scanf("%i", &QNH);
-
-    QNHDiff = QNH - standardPressure;
-
-    printf("The difference is %i hPa", absoluteValue(QNHDiff));
-
-    // Get the aerodrome elevation
-    printf("\nEnter the aerodrome elevation (feet AMSL): ");
-    scanf("%i", &aerodromeElevation);
-
-    // Calculate the pressure altitude
-    printf("\nPressure Altitude %i ft", pressureAltitude = aerodromeElevation - QNHDiff * 30);
-
-    calcDensityAltitude();
-}
-
-void calcDensityAltitude(void)
-{
-    int surfaceTemp;
-    int tempDifference;
-    int tempDeviation;
-    int aerodromeISA;
-
-    printf("\n\nDENSITY ALTITUDE\n");
-
-    // Get the surface temperature
-    printf("\nEnter surface temperature (Celcius): ");
-    scanf("%i", &surfaceTemp);
-
-    // Calculate temperature variation
-    tempDifference = surfaceTemp - standardTemperature;
-
-    // Calculate temperature deviation
-    aerodromeISA = standardTemperature - (2 * aerodromeElevation / 1000);
-    tempDeviation = surfaceTemp - aerodromeISA;
-
-    // Calculate the density altitude
-    densityAltitude = pressureAltitude + (tempDeviation * 120);
-
-    printf("\nDensity Altitude: %i ft", densityAltitude);
-
-    showOptions();
 }
 
 int main(void)
 {
-    startMenu();
+    mainMenu();
 
     return 0;
 }
